54ff0f3b249e0775b403233d487528a0ab3cfa52
Peter Kinnaird <peter@kinnaird.scot>
2025-03-29 21:22:56 +0000

    Add pnpm workspace and vite config

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+export default defineConfig({
+  build: {
+    outDir: 'browser',
+    sourcemap: true,
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'TypescriptCliTemplate',
+      fileName: 'typescript-cli-template'
+    },
+    // We're not using any external dependencies, but these options are important for these
+    rollupOptions: {
+      external: [],
+      output: {
+        globals: {}
+      }
+    }
+  }
+})
