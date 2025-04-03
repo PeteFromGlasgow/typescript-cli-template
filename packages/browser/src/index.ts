11a2d147532ae7613dd60e300d7b7f2bcb0abe24
Peter Kinnaird <peter@kinnaird.scot>
2025-04-04 00:53:50 +0100

    Add in a browser library example with a test

---
+++ packages/browser/src/index.ts
@@ -0,0 +1,11 @@
+import { getHelloWorld } from '@typescript-cli-template/core'
+
+export function bindField(fieldId: string, targetId: string) {
+  const field = document.getElementById(fieldId)
+  const target = document.getElementById(targetId)
+
+  if (field && target) {
+    const sayHello = getHelloWorld((name) => target.innerHTML = name)
+    field.addEventListener('keyup', event => sayHello((event.target as any).value))
+  }
+}
---
+++ packages/browser/test/helloWorld.test.ts
@@ -0,0 +1,22 @@
+import { describe, it, expect } from 'vitest'
+import { page, userEvent } from '@vitest/browser/context'
+import { bindField } from '../src/index.js'
+
+describe('helloWorld', () => {
+  it('Should return "Hello Ben!" when given "Ben" as an argument', async () => {
+
+    const content = `
+      <input id='in' type='text'></input>
+      <div id='out'></div>
+    `;
+
+    document.body.innerHTML = content
+
+    bindField('in', 'out')
+    const input = document.getElementById('in')
+    const output = document.getElementById('out')
+
+    await userEvent.type(document.querySelector('#in')!, 'Ben')
+    expect(output?.innerHTML).to.equal('Hello Ben!')
+  })
+})
