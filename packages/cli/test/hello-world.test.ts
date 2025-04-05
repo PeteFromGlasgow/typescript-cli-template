5dbc9b8e210e333330e8b176d512ec218441e5b1
Peter Kinnaird <peter@kinnaird.scot>
2025-04-05 21:17:20 +0100

    Get a working docker build

--- packages/cli/test/hello-world.test.ts
+++ packages/cli/test/hello-world.test.ts
@@ -4,7 +4,7 @@ import { getProgram } from '../src/cli.js'
 describe('Command hello-world <name>', () => {
   it('Should return "Hello George!" when given "George" as an argument', async () => {
     let result: string | null = null
-    const program = await getProgram(i => result = i)
+    const program = await getProgram((i) => result = (i as string))
     await program.parseAsync(['node', 'src/app.js', 'hello-world', 'George'])
     expect(result).to.equal('Hello George!')
   })
