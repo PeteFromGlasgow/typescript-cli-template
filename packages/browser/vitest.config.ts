021f542cbc442da7b9894e4eaa826b3a5c013741
Peter Kinnaird <peter@kinnaird.scot>
2025-04-09 22:11:04 +0100

    Make browser tests headless

--- packages/browser/vitest.config.ts
+++ packages/browser/vitest.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
     browser: {
       provider: 'playwright',
       enabled: true,
+      headless: true,
       // at least one instance is required
       instances: [
         { browser: 'chromium' }
